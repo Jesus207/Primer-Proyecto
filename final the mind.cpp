e637856b64db7ad48c7d1deb32aab7ac36fb3c3b
Jesus207 <64297332+Jesus207@users.noreply.github.com>
2026-03-22 05:02:28 +0000

    mn

--- final the mind.cpp
+++ final the mind.cpp
@@ -10,3 +10,4 @@ public:
     char mapa[FILAS][COLUMNAS];
     vector<pair<int, int>> pst_cts_p1={{1,1},{1,3},{1,5},{1,7},{1,9}};
     vector<pair<int, int>> pst_cts_p2={{18,1},{18,3},{18,5},{18,7},{18,9}};
+};
